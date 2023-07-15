52b94eb98014436215a0021b12c0736ec36aefd0
GuadalupeBaigorria2 <guabaiegu@gmail.com>
2023-07-14 21:11:17 -0300

    Add DB Context

---
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) :
+       base(options)
+        {
+
+        }
+    }
+}
+
